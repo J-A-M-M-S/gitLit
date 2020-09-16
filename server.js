aa2807685cc09e59a774310a1729f46f238d11d6
marlon20m <marlonmora.ndr@gmail.com>
2020-09-16 18:05:39 -0500

    replacing express.static public with client/build server.js line 13

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
+app.use(express.static("client/build"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useUnifiedTopology: true,
