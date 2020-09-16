2ff34272326e100ffe95df5481fc8b9a8d1a5daa
marlon20m <marlonmora.ndr@gmail.com>
2020-09-15 21:59:25 -0500

    adding useUnifiedTopology to server.js to avoid heroku issues

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const db = require('./models');
 const environment = process.env.NODE_ENV || 'development';
 const mongoose = require("mongoose");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,6 +13,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useUnifiedTopology: true,
   useNewUrlParser: true,
   useFindAndModify: false
 });
