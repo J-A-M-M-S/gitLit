7022a410287dbc83d740c51d0e29d0cabeed049b
AshleyLerma <lerma.scout@gmail.com>
2020-09-20 18:45:29 -0500

    added title test

---
+++ client/src/components/Title.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+// import react-testing methods
+import { render } from "@testing-library/react";
+// add custom jest matchers from jest-dom
+import "@testing-library/jest-dom/extend-expect";
+// import Table component
+import Title from "./Title";
+
+describe("Table", () => {
+  test("loads and displays title of app", async () => {
+    // Arrange
+    const { container } = render(<Title />);
+    // Act - user is not interacting with this
+    // Assert
+    expect(container).toContainHTML("gitLit");
+  });
+});
