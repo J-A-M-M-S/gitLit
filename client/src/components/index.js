bcf56f26e1de08477478f98246c3522c1d2d2fa1
Michael Srithapin <michael.srithapin@gmail.com>
2020-09-21 21:34:15 -0500

    attempting to create css sheet

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,7 +2,7 @@ import Content from "./content";
 import Footer from "./footer";
 import Hero from "./hero";
 import Loading from "./loading";
-import NavBar from "./nav-bar";
+import NavBar from "./Nav-bar";
 import Highlight from "./highlight";
 import PrivateRoute from "./private-route";
 
