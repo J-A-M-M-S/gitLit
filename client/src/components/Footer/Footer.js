9daf9ea6979f636cb9e823f14c9653e2a9902e56
Joshua <joshua.bogart.ndt@gmail.com>
2020-09-22 20:25:48 -0500

    added clickable social icons to the footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,12 +1,41 @@
 import React from "react";
+import "./Footer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faYoutube,
+  faFacebook,
+  faTwitter,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => (
   <footer className="bg-light p-3 text-center">
     <div className="logo" />
-    <p>
-      Sample project provided by{" "}
-      <a target="_blank" rel="noopener noreferrer" href="https://auth0.com">
-        Auth0
+    <p class="social-container">
+      <h3>Like, Follow, Subscribe</h3>
+      <a
+        href="https://www.youtube.com/c/jamesqquick"
+        className="youtube social"
+      >
+        <FontAwesomeIcon icon={faYoutube} size="2x" />
+      </a>
+      <a
+        href="https://www.facebook.com/learnbuildteach/"
+        className="facebook social"
+      >
+        <FontAwesomeIcon icon={faFacebook} size="2x" />
+      </a>
+      <a
+        href="https://www.twitter.com/jamesqquick"
+        className="twitter social"
+      >
+        <FontAwesomeIcon icon={faTwitter} size="2x" />
+      </a>
+      <a
+        href="https://www.instagram.com/learnbuildteach"
+        className="instagram social"
+      >
+        <FontAwesomeIcon icon={faInstagram} size="2x" />
       </a>
     </p>
   </footer>
