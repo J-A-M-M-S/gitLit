e2cad9c1da604f4f4c7cd19bcfec7a63c2237b33
AshleyLerma <lerma.scout@gmail.com>
2020-09-22 20:11:59 -0500

    Capitalize highlight and loading

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,7 +1,7 @@
 import Footer from "./footer";
-import Loading from "./loading";
+import Loading from "./Loading";
 import NavBar from "./Navbar/Navbar";
-import Highlight from "./highlight";
+import Highlight from "./Highlight";
 import PrivateRoute from "./private-route";
 
 export { Footer, Loading, NavBar, Highlight, PrivateRoute };
--- client/src/views/external-api.js
+++ client/src/views/external-api.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Button, ButtonGroup, Container } from "react-bootstrap";
-import Highlight from "../components/highlight";
+import Highlight from "../components/Highlight";
 
 export const ExternalApi = () => {
   const [message, setMessage] = useState("");
@@ -9,9 +9,9 @@ export const ExternalApi = () => {
     <Container className="mb-5">
       <h1>External API</h1>
       <p>
-        You use will use a button to call an external API using an access token,
-        and the API will validate it using the API's audience value.{" "}
-        <strong>This route should be private</strong>.
+        You use will use a button to call an external API using an
+        access token, and the API will validate it using the API's
+        audience value. <strong>This route should be private</strong>.
       </p>
       <ButtonGroup>
         <Button color="primary" className="mt-5">
