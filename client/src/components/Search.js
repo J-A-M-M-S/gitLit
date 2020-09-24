549cc75d2dd58b9613e4e2151b63d273e7910811
Michael Srithapin <michael.srithapin@gmail.com>
2020-09-24 18:41:55 -0500

    moved the search component to drink search page

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import API from "../utils/api";
+import { InputGroup, FormControl } from "react-bootstrap";
 
 function Search() {
   const drink = "margarita";
@@ -7,11 +8,26 @@ function Search() {
   useEffect(() => {
     API.searchDrinks(drink).then((results) => {
       setdata(results.data);
+      console.log(results);
     });
   }, []);
+
   // add search box and add API to searchbox function.
   return (
     <div>
+      <InputGroup className="mb-3">
+        <InputGroup.Prepend>
+          <InputGroup.Text id="basic-addon1">
+            Search Cocktail
+          </InputGroup.Text>
+        </InputGroup.Prepend>
+        <FormControl
+          placeholder="name a drink"
+          aria-label="name a drink"
+          aria-describedby="basic-addon1"
+        />
+      </InputGroup>
+
       {data.map((drink) => (
         <p key={drink.id}>{drink.name}</p>
       ))}
