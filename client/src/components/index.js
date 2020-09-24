6c74f5b79516ae04b587530c7d075dae20c1122e
Joshua <joshua.bogart.ndt@gmail.com>
2020-09-23 20:27:31 -0500

    updated with current origin/master

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-import Content from "./content";
-import Footer from "./Footer/Footer";
-import Hero from "./hero";
-import Loading from "./loading";
-=======
 import Content from "./Content/Content";
 import Footer from "./Footer/Footer";
 import Hero from "./Hero/Hero";
 import Loading from "./Loading/Loading";
->>>>>>> origin/master
 import NavBar from "./Navbar/Navbar";
 import Highlight from "./Highlight/Highlight";
 import PrivateRoute from "./PrivateRoute/PrivateRoute";
