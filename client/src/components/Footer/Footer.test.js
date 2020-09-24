ee86db40fa495fc98d43fbe8c91851328bbacf8c
Joshua <joshua.bogart.ndt@gmail.com>
2020-09-23 20:21:18 -0500

    trying to get a basic test to run on footer

--- client/src/components/Footer/Footer.test.js
+++ client/src/components/Footer/Footer.test.js
@@ -1,34 +1,17 @@
 import React from "react";
+// import react-testing methods
 import { render } from "@testing-library/react";
+// add custom jest matchers from jest-dom
 import "@testing-library/jest-dom/extend-expect";
+// import Footer component
 import Footer from "./Footer";
-// you can also use a renderer like "@testing-library/react" or "enzyme/mount" here
-import { BrowserRouter as Router } from "react-router-dom";
-import { act } from "react-dom/test-utils";
 
-// app.test.js
-test("navigates social when you click the icon", () => {
-  // in a real test a renderer like "@testing-library/react"
-  // would take care of setting up the DOM elements
-  //   const root = document.createElement("div");
-  //   document.body.appendChild(root);
-
-  // Render app
-  render(
-    <Router>
-      <Footer />
-    </Router>,
-    // root,
-  );
-
-  // Interact with page
-  act(() => {
-    // Find the link (perhaps using the text content)
-    const goYouTubeLink = document.querySelector(".youtube");
-    // Click it
-    goYouTubeLink.dispatchEvent(new MouseEvent("click", {}));
+describe("Footer", () => {
+  test("loads and displays social items", () => {
+    // Arrange
+    const { container } = render(<Footer />);
+    // Act - user is not interacting with this
+    // Assert
+    expect(container).toContainHTML("Like, Follow, Subscribe");
   });
-
-  // Check correct page content showed up
-  expect(document.body.baseURI).toBe("https://www.youtube.com/");
 });
