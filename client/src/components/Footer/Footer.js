36fc74f4325abbc6e32db410b2fa3cfa029d9b61
Joshua <joshua.bogart.ndt@gmail.com>
2020-09-24 20:05:32 -0500

    return

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -8,23 +8,28 @@ import {
   faInstagram,
 } from "@fortawesome/free-brands-svg-icons";
 
-const Footer = () => (
-  <footer className="header">
-    <div className="logo" />
-    <h3>Like, Follow, Subscribe</h3>
-    <a href="https://www.youtube.com/" className="youtube social">
-      <FontAwesomeIcon icon={faYoutube} size="2x" />
-    </a>
-    <a href="https://www.facebook.com/" className="facebook social">
-      <FontAwesomeIcon icon={faFacebook} size="2x" />
-    </a>
-    <a href="https://www.twitter.com/" className="twitter social">
-      <FontAwesomeIcon icon={faTwitter} size="2x" />
-    </a>
-    <a href="https://www.instagram.com/" className="instagram social">
-      <FontAwesomeIcon icon={faInstagram} size="2x" />
-    </a>
-  </footer>
-);
+const Footer = () => {
+  return (
+    <footer className="header">
+      <div className="logo" />
+      <h3>Like, Follow, Subscribe</h3>
+      <a href="https://www.youtube.com/" className="youtube social">
+        <FontAwesomeIcon icon={faYoutube} size="2x" />
+      </a>
+      <a href="https://www.facebook.com/" className="facebook social">
+        <FontAwesomeIcon icon={faFacebook} size="2x" />
+      </a>
+      <a href="https://www.twitter.com/" className="twitter social">
+        <FontAwesomeIcon icon={faTwitter} size="2x" />
+      </a>
+      <a
+        href="https://www.instagram.com/"
+        className="instagram social"
+      >
+        <FontAwesomeIcon icon={faInstagram} size="2x" />
+      </a>
+    </footer>
+  );
+};
 
 export default Footer;
