d0b860e77e14030be3b50f0a4d4f26c926af7ef5
juanlajara <juanlajara001@gmail.com>
2020-09-24 19:01:54 -0500

    + view index import

--- client/src/views/index.js
+++ client/src/views/index.js
@@ -1,6 +1,6 @@
 import Home from "./Home";
 import Profile from "./Profile";
-import DrinkSearch from "./DrinkSearchtst";
+import DrinkSearch from "./DrinkSearch";
 import Mixology from "./Mixology";
 import Roulette from "./Roulette";
 
