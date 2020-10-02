d3d8885547f36ad1388f9e5403e7007ec6734e70
Marlon Mora <marlonmora.ndr@gmail.com>
2020-10-02 19:20:50 -0300

    testing enzyme but continuous .map() error

--- client/src/components/Search/Search.test.js
+++ client/src/components/Search/Search.test.js
@@ -1,15 +1,28 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { mount } from "enzyme";
+import { act } from "react-dom/test-utils";
+import waitForExpect from "wait-for-expect";
 import Search from "./Search";
+import { BrowserRouter as Router } from "react-router-dom";
 
-const testClick = (link, id) => {
-	const { getByTestId } = render(<Router>{link}</Router>);
-	fireEvent.click(getByTestId(id));
-};
 
-test("should click Submit button ", () => {
-	testClick(<Search />, "testClick");
+
+describe("<Search />", () => {
+	test("download ingredients from internets", async () => {	
+  const wrapper = mount(
+    <Router><Search data={Search} /></Router>
+  );
+
+  await act(async () => {
+    wrapper.find({ children: "Submit" }).simulate("click");
+  });
+
+  await waitForExpect(() => {
+    wrapper.update();
+    data.forEach(data => {
+      expect(wrapper.text()).toMatch(data);
+    });
+  });
 });
 
-
+});
