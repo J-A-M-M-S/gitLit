2dd7d2952dcaf5229a4bf7462665112a67a192b6
AshleyLerma <lerma.scout@gmail.com>
2020-10-03 13:44:47 -0500

    Create RandomButtons Component

--- client/src/views/Roulette.js
+++ client/src/views/Roulette.js
@@ -1,7 +1,12 @@
 import React from "react";
+import DrinkDetails from "../components/DrinkDetails/DrinkDetails";
 
 const Roulette = () => {
-  return <h1>Roulette Insert UX/UI Here</h1>;
+  return (
+    <div>
+      <h1>Roulette Insert UX/UI Here</h1>
+    </div>
+  );
 };
 
 export default Roulette;
