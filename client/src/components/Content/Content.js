1611f839ef26de05c188f9d26ad1edf253d1588f
juanlajara <juanlajara001@gmail.com>
2020-10-03 22:36:43 -0500

    - row from react

--- client/src/components/Content/Content.js
+++ client/src/components/Content/Content.js
@@ -1,7 +1,5 @@
 import React from "react";
 
-import { Row } from "react-bootstrap";
-
 const Content = () => (
   <div className="next-steps my-5">
     <h2 className="my-5 text-center">What can I do next?</h2>
