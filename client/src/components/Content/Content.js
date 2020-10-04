33855f7fdca98e740a4ae34fcf8bfa3c750efb16
juanlajara <juanlajara001@gmail.com>
2020-10-03 22:30:46 -0500

    - content with lorem ipsum

--- client/src/components/Content/Content.js
+++ client/src/components/Content/Content.js
@@ -5,9 +5,6 @@ import { Row } from "react-bootstrap";
 const Content = () => (
   <div className="next-steps my-5">
     <h2 className="my-5 text-center">What can I do next?</h2>
-    <Row className="d-flex justify-content-between">
-      <p>lorem ipsum</p>
-    </Row>
   </div>
 );
 
