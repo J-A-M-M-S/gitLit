59fd290047df1c186a05e950fde44a74bc1697a0
Marlon Mora <marlonmora.ndr@gmail.com>
2020-10-05 09:42:39 -0300

    created a use effect for all cocktail names

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import DrinkSearch from "../../views/DrinkSearch"
+import API from "../../utils/api";
 import {
   Container,
   Row,
@@ -6,6 +8,7 @@ import {
   InputGroup,
   FormControl,
 } from "react-bootstrap";
+import { Typeahead } from 'react-bootstrap-typeahead'; // ES2015
 import "./Search.css";
 
 function Search({
@@ -15,6 +18,16 @@ function Search({
   searchCocktail,
   drinkDeets,
 }) {
+
+  const [singleSelections, setSingleSelections] = useState([]);
+
+  useEffect(()=>{
+    API.allCocktailNames().then((results) => {
+      setSingleSelections(results.data);
+    });
+    console.log(results.data)
+  },[])
+
   return (
     <div className="searchRes">
       <Container>
